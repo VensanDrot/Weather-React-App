f915f3b1b9a68d41ca4ba4a06abdc4f5217d58a8
Vensan <v.drot@student.inha.uz>
2022-07-03 03:45:49 +0500

    building the form

--- src/components/Forecast/Fstyle.jsx
+++ src/components/Forecast/Fstyle.jsx
@@ -4,7 +4,9 @@ import styled from "styled-components";
 export const container = styled.div`
 margin: 10rem 15rem;
 background: #000000;
-    
+border: 1px solid transperent;
+border-radius: 1.5rem;
+padding: 1.5rem;
 `
 
 export const maincontainer = styled.div`
@@ -14,6 +16,8 @@ export const maincontainer = styled.div`
     text-align:center;
     height:50vh;
     position:relative;
+    
+    
     `
 
 export const h1= styled.h1`
@@ -26,13 +30,21 @@ export const h1= styled.h1`
 export const day_cont = styled.div`
     display:grid;
     grid-template-rows: 25% 40% 25%; 
-    
+    border: 1px solid transperent;
+    border-radius: 5rem;
     row-gap: 5%;
     color:#ffffff;
+    position:relative;
+    transition: 0.3s;
+    &:hover {
+        background: #aaccee;
+      }
 `
 
 export const p = styled.p`
 background: #ff00aa;
+width:85%;
+margin-left: 7.5%;
 display:flex;
 align-items:center;
 justify-content:center;
