6d5eb45ec766c80b244a92a4d2140f482c1eaa3b
Vensan <v.drot@student.inha.uz>
2022-07-04 03:56:21 +0500

    weather api connected

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import * as S from './Fstyle'
-
+import APIW from '../Api/APIW'
 
 const Forecast = () => {
-    
+  const fetchWeather = async() => {
+    const data =  await APIW();
+    console.log(data);
+}
+fetchWeather();
   return (
     <S.container>
         <S.h1>Forecast for every 3 hours</S.h1>
--- src/components/Forecast/Fstyle.jsx
+++ src/components/Forecast/Fstyle.jsx
@@ -2,37 +2,42 @@ import styled from "styled-components";
 
 
 export const container = styled.div`
-margin: 10rem 15rem;
-background: #000000;
+background: rgb(15, 14, 14);
 border: 1px solid transperent;
 border-radius: 1.5rem;
-padding: 1.5rem;
+height: 750px;
+width: 750px;
+margin: 3rem auto;
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
 `
 
 export const maincontainer = styled.div`
     display:grid;
     grid-template-columns:  1fr 1fr 1fr 1fr 1fr;
-    column-gap: 1.6rem;
     text-align:center;
-    height:50vh;
     position:relative;
-    
-    
+    height:80%;  
+   
     `
 
 export const h1= styled.h1`
     text-align:center;
-    font-size:2.4rem;
+    font-size:1.7rem;
     color:#fff;
 `
 
 
 export const day_cont = styled.div`
     display:grid;
+    
     grid-template-rows: 25% 40% 25%; 
     border: 1px solid transperent;
-    border-radius: 5rem;
+    border-radius: 1.5rem;
     row-gap: 5%;
+    width:130px;
     color:#ffffff;
     position:relative;
     transition: 0.3s;
