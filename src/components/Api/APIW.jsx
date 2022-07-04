c8fc8c520c85a425020c7119b016679ba57dff47
Vensan <v.drot@student.inha.uz>
2022-07-04 16:05:08 +0500

    weather api connected

--- src/components/Api/APIW.jsx
+++ src/components/Api/APIW.jsx
@@ -1,25 +1,29 @@
 const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=41&lon=69&appid=63af4b12863f964b7dd0c84b2c1ef94a&units=metric&cnt=5';
 
 
+
+
 async function APIW() {
-  return fetch(url)
+  const result = await fetch(url)
   .then((res)=>res.json())
   .then((data) => {
-    const days = [];
+   const days = [];
 
-    data.list.map((day) => {
-      days.push({
-        date: day.slice(5, 10),
-        time: day.slice(11, 16),
-        wind: day.wind.speed,
-        dayTemp: day.main.temp_max,
-        nightTemp: day.main.temp_min,
-        weather: day.weather[0].main,
-      });
-    });
-    return days;
-    });
-    
+   data.list.map((day) => {
+     days.push({
+       date: day.dt_txt.slice(5, 10),
+       time: day.dt_txt.slice(11, 16),
+       wind: day.wind.speed,
+       dayTemp: day.main.temp_max,
+       nightTemp: day.main.temp_min,
+       weather: day.weather.main,
+     });
+   });
+  
+   return days;
+  });
+
+    return result;
   }
 
 
--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import * as S from './Fstyle'
 import APIW from '../Api/APIW'
 const days = [];
 const Forecast = () => {
-  const fetchWeather = async() => {
-    const data =  await APIW();
-   return data;
-}
-  fetchWeather()
- 
+  
 
+
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    APIW().then((data) => {
+      setDays(data);
+    });
+  }, []);
+console.log(days);
 return (
     <S.container>
         <S.h1>Forecast for every 3 hours</S.h1>
         <S.maincontainer>
-       
-        <S.day_cont><S.p>12</S.p><S.p>12</S.p><S.p>12</S.p></S.day_cont>
-        <S.day_cont><S.p>12</S.p><S.p>12</S.p><S.p>12</S.p></S.day_cont>
-        <S.day_cont><S.p>12</S.p><S.p>12</S.p><S.p>12</S.p></S.day_cont>
-        <S.day_cont><S.p>12</S.p><S.p>12</S.p><S.p>12</S.p></S.day_cont>
-        <S.day_cont><S.p>12</S.p><S.p>12</S.p><S.p>12</S.p></S.day_cont>
+        {days.length!==0 && days.map((g)=> {
+          return ( <S.day_cont><S.p>{g.date}</S.p><S.p>12</S.p><S.p>12</S.p></S.day_cont>)
+        })}
+          <S.day_cont><S.p>12</S.p><S.p>12</S.p><S.p>12</S.p></S.day_cont>
         </S.maincontainer>
     </S.container>
   )
